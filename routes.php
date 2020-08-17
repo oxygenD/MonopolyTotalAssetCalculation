88671febc1c0873f82201ee0bafd6e0be272ae17
oxygenD <readingbookisimportant@yahoo.co.jp>
2020-08-17 22:23:55 +0900

    コントローラクラスに資産登録表示と資産登録決定のルーティングを追記した

--- routes.php
+++ routes.php
@@ -4,3 +4,7 @@ use SocymSlim\Monopoly\controllers\PlayerController;
 $app->any("/goPlayerAdd", PlayerController::class.":goPlayerAdd");
 $app->any("/playerAdd", PlayerController::class.":playerAdd");
 $app->any("/showPlayerList", PlayerController::class.":showPlayerList");
+// プレイヤーの資産情報を登録する画面を表示する
+$app->any("/goRegisterAssetInfo/{id}", PlayerController::class.":goRegisterAssetInfo");
+// プレイヤーの資産情報を登録する
+$app->any("/RegisterAssetInfo/{id}", PlayerController::class.":RegisterAssetInfo");
