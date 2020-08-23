21dad6ed708568694d7bbff2ffd0b435315f5d99
oxygenD <readingbookisimportant@yahoo.co.jp>
2020-08-23 22:39:27 +0900

    #4 物件情報を一覧表示できるようにした

--- classes/controllers/PlayerController.php
+++ classes/controllers/PlayerController.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Container\ContainerInterface;
 use SocymSlim\Monopoly\entities\Player;
 use SocymSlim\Monopoly\daos\PlayerDAO;
+use SocymSlim\Monopoly\daos\PropertyDAO;
 use SocymSlim\Monopoly\exceptions\DataAccessException;
 
 class PlayerController
@@ -123,14 +124,48 @@ class PlayerController
         return $response;
     }
 
-    // 資産登録画面を表示する
+    // 資産登録画面を表示する。※条件付き物件一覧画面とも言える。現状、システムの規模が小さいので良いが。
     public function goRegisterAssetInfo(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
+
+        // 表示先テンプレートのファイルパス
+        $templatePath = "AssetInfoRegister.html";
+
+        // テンプレート変数を格納する連想配列
+        $assign = [];
+        // URL中のパラメータを取得
+        $playerId = $args["id"];
+
+        try {
+            // PDOインスタンスをコンテナから取得
+            $db = $this->container->get("db");
+            $propertyDAO = new PropertyDAO($db);
+
+            $property = $propertyDAO->find($playerId);
+
+            
+            // データが存在する場合
+            if (isset($property)) {
+
+                // テンプレート変数としてMemberエンティティを格納
+                $assign["propertyList"] = $property;
+            } else {
+                $assign["msg"] = "閲覧できる物件情報は存在しません。";
+            }
+
+        } catch(PDOException $ex) {
+            $exCode = $ex->getCode();
+            throw new DataAccessException("データベース処理中に障害が発生しました。", $exCode, $ex);
+
+        } finally {
+            // DB切断
+            $db = null;
+        }
+
         // Twigインスタンスをコンテナから取得
         $twig = $this->container->get("view");
-
         // レスポンスオブジェクトの作成
-        $response = $twig->render($response, "AssetInfoRegister.html");
+        $response = $twig->render($response, $templatePath,$assign);
 
         return $response;
     }
---
+++ classes/daos/PropertyDAO.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SocymSlim\Monopoly\daos;
+
+use PDO;
+use SocymSlim\Monopoly\entities\Property;
+
+class PropertyDAO
+{
+    // PDOインスタンスを表すプロパティ
+    private $db;
+
+    // コンストラクタ
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    
+    // プレイヤーが保有するか、或いは誰も保有していない物件を取得する。
+    // 適切な命名を考えること
+    public function find(int $id): array
+    {
+        // プレイヤーが所有しているか、もしくは売れていない物件リストを格納する
+        $propertyList = [];
+        // データ取得用SQLを文字列で用意。
+        // 画面アクセスしたプレイヤーが所有しているか、誰も所有していない物件を条件とする。
+        // $sqlSelect = "SELECT * FROM properties WHERE player_id = $id or null ORDER BY id";
+        $sqlSelect = "SELECT * FROM properties";
+        // プリペアードステートメントインスタンスを取得
+        $stmt = $this->db->prepare($sqlSelect);
+        // SQLの実行
+        $result = $stmt->execute();
+
+        // SQL実行が成功したとき
+        if ($result) {
+
+            while ($row = $stmt->fetch()) {
+                // 各カラムのデータ取得
+                $id = $row["property_id"];
+                $name = $row["property_name"];
+                $basicPrice = $row["basic_price"];
+                $playerId = $row["player_id"];
+                
+                // Propertyエンティティインスタンスを生成
+                $property = new Property();
+
+                $property->setPropertyId($id);
+                $property->setName($name);
+                $property->setBasicPrice($basicPrice);// データベースには必要だが、物件一覧で必要か？
+                $property->setPlayerID($playerId);
+
+                // propertyエンティティを物件リスト連想配列に格納
+                $propertyList[$id] = $property;
+            }
+        }
+
+        return $propertyList;
+    }
+}
