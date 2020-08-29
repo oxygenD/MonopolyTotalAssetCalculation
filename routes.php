821e9016d776dd3fc10d6de1f64633e34e7d3eaa
oxygenD <readingbookisimportant@yahoo.co.jp>
2020-08-29 22:42:40 +0900

    #4 リファクタリング。責務を考え、controllerの処理をドメインベースで分離した

--- routes.php
+++ routes.php
@@ -1,10 +1,11 @@
 <?php
 use SocymSlim\Monopoly\controllers\PlayerController;
+use SocymSlim\Monopoly\controllers\PropertyController;
 
 $app->any("/goPlayerAdd", PlayerController::class.":goPlayerAdd");
 $app->any("/playerAdd", PlayerController::class.":playerAdd");
 $app->any("/showPlayerList", PlayerController::class.":showPlayerList");
 // プレイヤーの資産情報を登録する画面を表示する
-$app->any("/goUpdateAssetInfo/{id}", PlayerController::class.":goUpdateAssetInfo");
+$app->any("/goUpdateAssetInfo/{id}", PropertyController::class.":goUpdateAssetInfo");
 // プレイヤーの資産情報を登録する
-$app->any("/UpdateAssetInfo/{id}", PlayerController::class.":UpdateAssetInfo");
+$app->any("/UpdateAssetInfo/{id}", PropertyController::class.":UpdateAssetInfo");
