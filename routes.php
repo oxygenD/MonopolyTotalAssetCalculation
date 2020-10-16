c25cd7b74e322044a37a18675ca46ac52b177721
oxygenD <readingbookisimportant@yahoo.co.jp>
2020-10-16 22:02:22 +0900

    #4 登録画面の初期表示までの実装なので、更新処理のルーティングは一旦、削除した

--- routes.php
+++ routes.php
@@ -7,5 +7,3 @@ $app->any("/playerAdd", PlayerController::class.":playerAdd");
 $app->any("/showPlayerList", PlayerController::class.":showPlayerList");
 // プレイヤーの資産情報を登録する画面を表示する
 $app->any("/goUpdateAssetInfo/{id}", PropertyController::class.":goUpdateAssetInfo");
-// プレイヤーの資産情報を登録する
-$app->any("/UpdateAssetInfo/{id}", PropertyController::class.":UpdateAssetInfo");
